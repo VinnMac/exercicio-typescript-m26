f8afad253b5906c2fa09065557de6ea65e18d295
Vinícius Machado <vinicius.machadoo@outlook.com>
2026-05-07 11:42:38 -0300

    commit inicial

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiplicaValores = (a, b) => a * b;
+const saldacaoUsuario = (nomeUsuario) => `Olá, ${nomeUsuario}.`;
+console.log(multiplicaValores(2, 4));
+console.log(saldacaoUsuario("Jorge"));
+//# sourceMappingURL=exercicio.js.map
---
+++ exercicio.ts
@@ -0,0 +1,6 @@
+const multiplicaValores = (a: number, b: number) : number => a * b;
+
+const saldacaoUsuario = (nomeUsuario: string) : string => `Olá, ${nomeUsuario}.`;
+
+console.log(multiplicaValores(2, 4));
+console.log(saldacaoUsuario("Jorge"));
