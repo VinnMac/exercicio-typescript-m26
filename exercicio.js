f713f1ff37abcecac20a1119e76fd6484518744e
Vinícius Machado <vinicius.machadoo@outlook.com>
2026-05-07 11:53:17 -0300

    remocao do exercicio.js

--- exercicio.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const multiplicaValores = (a, b) => a * b;
-const saldacaoUsuario = (nomeUsuario) => `Olá, ${nomeUsuario}.`;
-console.log(multiplicaValores(2, 4));
-console.log(saldacaoUsuario("Jorge"));
-//# sourceMappingURL=exercicio.js.map
